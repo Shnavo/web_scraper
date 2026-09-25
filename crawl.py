50a0b002db5dc582bb0495665eba22f386354658
Shnavo <szango297@gmail.com>
2026-09-25 15:39:35 +0200

    Completed base crawling application

--- crawl.py
+++ crawl.py
@@ -1,6 +1,8 @@
 from typing import TypedDict
 from urllib.parse import urlsplit, urljoin
+
 from bs4 import BeautifulSoup, Tag
+import requests
 
 
 class PageData(TypedDict):
@@ -13,10 +15,8 @@ class PageData(TypedDict):
 
 def normalize_url(url: str) -> str:
     parsed = urlsplit(url)
-    if parsed.hostname != None:
-        edited = parsed.hostname + parsed.path.rstrip("/")
-        return edited.lower()
-    edited = f"www.{parsed.path.rstrip("/")}"
+    edited = f"{parsed.scheme}://{parsed.netloc}{parsed.path}"
+    edited = edited.rstrip("/")
     return edited.lower()
 
 
@@ -84,3 +84,56 @@ def extract_page_data(html: str, page_url: str) -> PageData:
         "outgoing_links": get_urls_from_html(html, page_url),
         "image_urls": get_images_from_html(html, page_url),
     }
+
+
+def get_html(url: str) -> str:
+    try:
+        resp = requests.get(url, headers={"User-Agent": "Crawler/1.0"})
+    except Exception as e:
+        raise Exception(f"network error while fetching {url}: {e}")
+
+    if resp.status_code > 399:
+        raise Exception(f"Client error response: {resp.status_code} {resp.reason}")
+
+    if "text/html" not in resp.headers["content-type"]:
+        raise Exception(f"Incorrect type of response: {resp.headers["content-type"]}")
+
+    return resp.text
+
+
+def crawl_page(
+    base_url: str,
+    current_url: str | None = None,
+    page_data: dict[str, PageData] | None = None,
+) -> dict[str, PageData]:
+
+    if current_url == None:
+        current_url = base_url
+    if page_data == None:
+        page_data = {}
+
+    p_base = urlsplit(base_url)
+    p_current = urlsplit(current_url)
+    if p_base.netloc != p_current.netloc:
+        return page_data
+
+    norm_url = normalize_url(current_url)
+
+    if norm_url in page_data:
+        return page_data
+
+    print(f"crawling {current_url}")
+    html = get_html(norm_url)
+    if html is None:
+        return page_data
+
+    data = extract_page_data(html, current_url)
+    page_data[norm_url] = data
+
+    if data["outgoing_links"] != []:
+        for next_url in data["outgoing_links"]:
+            if "email-protection" in next_url or next_url.endswith("jpg"):
+                continue
+            page_data = crawl_page(base_url, next_url, page_data)
+
+    return page_data
--- main.py
+++ main.py
@@ -1,5 +1,21 @@
+import sys
+from crawl import crawl_page
+
+
 def main():
-    print("Hello from web-scraper!")
+    if len(sys.argv) < 2:
+        print("no website provided")
+        sys.exit(1)
+    if len(sys.argv) > 2:
+        print("too many arguments provided")
+        sys.exit(1)
+    url = sys.argv[1]
+    print(f"starting crawl of: {url}\n")
+    data = crawl_page(url)
+    print(f"\nReturned {len(data)} sites\n")
+    print("Headings of each page:")
+    for page in data:
+        print(data[page]["heading"])
 
 
 if __name__ == "__main__":
